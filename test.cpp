e42b60f7af1b1754bf0a390048d848a7bb82103c
007jiang3 <jyx2103325827@126.com>
2024-12-28 22:05:53 +0800

    modify test

--- test.cpp
+++ test.cpp
@@ -2,16 +2,24 @@
 #include <iostream>
 #include <string>
 #include <optional>
+#include <vector>
+
+void test_in_place() {
+    nonstd::optional<std::string> a;
+    a.emplace("hello");
+    std::cout << *a << std::endl;
+
+    nonstd::optional<std::string> b(nonstd::in_place, "world");
+    std::cout << *b << std::endl;
+
+    nonstd::optional<std::vector<int>> c(nonstd::in_place, { 1, 2, 3 });
+    for (auto i : *c) {
+        std::cout << i << " ";
+    }
+}
 
 int main(int argc, char const* argv[]) {
-    nonstd::optional<int> a(1);
-    nonstd::optional<std::string> b("hello");
-    nonstd::optional<nonstd::nullopt_t> c;
-    nonstd::optional<double> d(0.1);
-
-    a == d;
-
-    a == c;
+    test_in_place();
 
     return 0;
 }
